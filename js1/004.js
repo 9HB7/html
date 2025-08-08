39ba92462d6d6e3ea3d06e4c3e8f5aed255a78f8
hbb <uhb9799@gmail.com>
2025-08-08 10:02:37 +0900

    html_test

---
+++ js1/004.js
@@ -0,0 +1,9 @@
+let number = "17";
+let name = "홍길동";
+let phone = "010-1111-2222";
+let address = "인천시 미추홀구 학익동 663-1";
+let 회원 = 
+{number:"17", name:"홍길동",phone:"010-1111-2222",address:"인천시 미추홀구 학익동 663-1"};
+
+console.log(회원);
+console.log(회원.address);
---
+++ js1/007.js
@@ -0,0 +1,16 @@
+const saram = {
+    irum:"홍길동",
+    nai:20,
+    insa(){
+        console.log(this.irum);
+    }
+};
+
+saram.insa();
+
+console.log(true+true);
+console.log(false+false);
+console.log(false+true);
+console.log(1);
+console.log(!1);
+console.log(!!1);
